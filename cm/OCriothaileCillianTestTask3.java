036b7e28948fbc907664b3f040232042c8c83260
me <cillian.critchley@gmail.com>
2019-02-27 11:56:05 +0000

    renamed java file for task 3

--- cm/OCriothaileCillianTestTask3.java
+++ cm/OCriothaileCillianTestTask3.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
-public class OCriothaileCillianTestTask2 {
+public class OCriothaileCillianTestTask3 {
 
     private  BigDecimal normalRate;
     private  BigDecimal reducedRate;
